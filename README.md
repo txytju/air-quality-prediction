6c929789ed2dc1ba0c03841cc2206258811c3182
txy <xingye_tian@tju.edu.cn>
2018-04-08 20:39:26 +0800

    [training data] generated training data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Data for this project can be found [here(password:my70)](https://pan.baidu.com/s/15q48jFovG4-s3y_lzeea5Q). Place the data folder in the root directory of the repo.
+
+# ChangeLog
+
+- 20180407 v0 初始数据分析
+- 20180408 v0.1 
+  - 增加了缺失值处理，处理方式暂为 `ffill`
+  - 生成了对应模型输入的训练数据
--- data_util.py
+++ data_util.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 from matplotlib import pyplot as plt
 
-def parse_bj_aq_data():
+def parse_bj_aq_data(fill_method="ffill"):
 	bj_aq_dataset_1 = pd.read_csv("./KDD_CUP_2018/Beijing/beijing_17_18_aq.csv")
 	bj_aq_dataset_2 = pd.read_csv("./KDD_CUP_2018/Beijing/beijing_201802_201803_aq.csv")
 	bj_aq_dataset = pd.concat([bj_aq_dataset_1, bj_aq_dataset_2], ignore_index=True)
@@ -40,12 +40,66 @@ def parse_bj_aq_data():
 
 	# a dict of station aq, Beijing
 	bj_aq_stations = {}
+	bj_aq_stations_noname = {}
 	for station in stations:
-	    bj_aq_stations[station] = bj_aq_dataset[bj_aq_dataset["stationId"]==station]
-	    bj_aq_stations[station] = bj_aq_stations[station].set_index("format_time")
+		bj_aq_station = bj_aq_dataset[bj_aq_dataset["stationId"]==station]
+		bj_aq_station.set_index("format_time", inplace=True)
+		bj_aq_station.drop("utc_time", axis=1, inplace=True)
+		bj_aq_station.drop("stationId", axis=1, inplace=True)
+
+		# rename
+		original_names = bj_aq_station.columns.values.tolist()
+		names_dict = {original_name : station+"_"+original_name for original_name in original_names}
+		bj_aq_station_renamed = bj_aq_station.rename(index=str, columns=names_dict)
+
+		# fill NaN
+		if fill_method == "ffill":
+			bj_aq_station_renamed.fillna(method="ffill", inplace=True)
+
+		bj_aq_stations[station] = bj_aq_station_renamed
 
 
-	return bj_aq_dataset, stations, bj_aq_stations
+	# 整合成一个大的 dataframe
+	bj_aq_stations_merged = pd.concat(list(bj_aq_stations.values()), axis=1)
+
+	return bj_aq_dataset, stations, bj_aq_stations, bj_aq_stations_merged
+
+def generate_model_data(merged_data, step, m):
+	'''
+	Input:
+		step : sample step.
+		m : batch size.
+	Return:
+		list of m batch size data.
+	'''
+
+	X_dataset = []
+	Y_dataset = []
+
+	model_length = 7 * 24
+	data_length = merged_data.shape[0]
+
+	for i in range(0,data_length - model_length, step):
+		X = merged_data.ix[i:i+5*24].values
+		Y = merged_data.ix[i+5*24:i+7*24].values
+		X = np.expand_dims(X, axis=0) # (1, Tx, feature_length)
+		Y = np.expand_dims(Y, axis=0) # (1, Ty, feature_length)
+		X_dataset.append(X) 
+		Y_dataset.append(Y)
+
+	X_batches = []
+	Y_batches = []
+	num = len(X_dataset) // m
+	for j in range(num):
+		X_batch = X_dataset[j*m:(j+1)*m]
+		Y_batch = Y_dataset[j*m:(j+1)*m]
+		X_batch = np.concatenate((X_batch), axis=0)
+		Y_batch = np.concatenate((Y_batch), axis=0)
+		X_batches.append(X_batch)
+		Y_batches.append(Y_batch)
+
+	return X_batches, Y_batches
+
 
 def plot_station(bj_aq_stations, station, feature, minimum=datetime.datetime(2017,1,1), maxmimum=datetime.datetime(2018,3,30)):
     data = bj_aq_stations[station].loc[minimum:maxmimum]
