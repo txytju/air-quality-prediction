457e1e684ef486f512e1ea9789d422721505d252
txy <xingye_tian@tju.edu.cn>
2018-04-14 21:12:17 +0800

    [lstm_predict_aq] add a lstm to predict single-feature aq

--- data_util.py
+++ data_util.py
@@ -171,6 +171,59 @@ def generate_model_data_v1(merged_data, step):
 # 	return a[p], b[p]
 
 
+def generate_toy_data_for_lstm(num_periods = 120, f_horizon = 4, samples = 10020):
+    '''
+    Generate toy data.
+    '''
+    # data  : t*sin(t)/3 + 2*sin(5*t)
+    t = np.linspace(0,100,num=samples)
+    ts = t*np.sin(t)/3 + 2.*np.sin(5.*t)
+    plt.plot(t,ts);
+    
+    TS = np.array(ts)
+
+    x_data = TS[:(len(TS)-(len(TS) % num_periods))]
+    y_data = TS[f_horizon : (len(TS)-(len(TS) % num_periods)+f_horizon)]
+    print("length of training data x : ", x_data.shape)
+    print("length of training data y : ", y_data.shape)
+
+    x_batches = x_data.reshape(-1,num_periods,1)
+    y_batches = y_data.reshape(-1,num_periods,1)
+
+    print("training data x shape : ", x_batches.shape)
+    
+    test_x_setup = TS[-(num_periods + f_horizon):]
+    testX = test_x_setup[:num_periods].reshape(-1,num_periods,1) 
+    testY = TS[-(num_periods):].reshape(-1,num_periods,1)
+    
+    return x_batches, y_batches, testX, testY
+
+
+def generate_data_for_lstm(ts, num_periods = 120, f_horizon = 4):
+    '''
+    ts : time series to be used.
+    '''
+    
+    TS = np.array(ts)
+
+    x_data = TS[:(len(TS)-(len(TS) % num_periods))]
+    y_data = TS[f_horizon : (len(TS)-(len(TS) % num_periods)+f_horizon)]
+    print("length of training data x : ", x_data.shape)
+    print("length of training data y : ", y_data.shape)
+
+    x_batches = x_data.reshape(-1,num_periods,1)
+    y_batches = y_data.reshape(-1,num_periods,1)
+
+    print("training data x shape : ", x_batches.shape)
+    
+    test_x_setup = TS[-(num_periods + f_horizon):]
+    testX = test_x_setup[:num_periods].reshape(-1,num_periods,1) 
+    testY = TS[-(num_periods):].reshape(-1,num_periods,1)
+    
+    return x_batches, y_batches, testX, testY
+
+
+
 
 def plot_station(bj_aq_stations, station, feature, minimum=datetime.datetime(2017,1,1), maxmimum=datetime.datetime(2018,3,30)):
     data = bj_aq_stations[station].loc[minimum:maxmimum]
--- model.py
+++ model.py
@@ -1,9 +1,17 @@
 import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
 
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 import keras.backend as K
+
+
 
 def softmax(x, axis=1):
     """Softmax activation function.
@@ -24,3 +32,66 @@ def softmax(x, axis=1):
         return e / s
     else:
         raise ValueError('Cannot apply softmax to a tensor that is 1D')
+
+
+def rnn_model(X_train, y_train, X_test, y_test, cell="BasicRNNCell", 
+              learning_rate=0.001, epochs=500, print_every=100, 
+              inputs=1, outputs=1, hidden=100, num_periods=20):
+    '''
+    X_tarin : x traning data.
+    y_train : y training data.
+    X_test : x data for prediction.
+    Y_test : ground truth of y data when doing prediction.
+    cell : which kind of rnn cell to use, "BasicRNNCell" for BasicRNNCell, "LSTMCell" for LSTMCell
+    learning_rate
+    inputs : input_shape
+    outputs : output_shape
+    hidden : number of hidden units
+    num_periods : length of time sequences.
+    '''
+    tf.reset_default_graph()
+
+    X = tf.placeholder(tf.float32, [None, num_periods, inputs])
+    y = tf.placeholder(tf.float32, [None, num_periods, outputs])
+
+    if cell == "BasicRNNCell" :
+        basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden, activation=tf.nn.relu)
+    elif cell == "LSTMCell" :
+        basic_cell = tf.contrib.rnn.LSTMCell(num_units=hidden, activation=tf.nn.relu)
+    else :
+        print("Cell model wrong.")
+        return
+
+    rnn_output, status = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)
+
+    stacked_rnn_output = tf.reshape(rnn_output, [-1, hidden])
+    stacked_outputs = tf.layers.dense(stacked_rnn_output, outputs)
+    outputs = tf.reshape(stacked_outputs, [-1, num_periods, outputs])
+
+    loss = tf.reduce_sum(tf.square(outputs - y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_op = optimizer.minimize(loss)
+
+    init = tf.global_variables_initializer()
+
+    losses = []
+    with tf.Session() as sess:
+        sess.run(init)
+        for ep in range(epochs):
+            _, loss_value = sess.run([training_op, loss], feed_dict={X:X_train, y:y_train})
+            losses.append(loss_value)
+            
+            if ep % print_every == 0:
+                mse = loss.eval(feed_dict={X:X_train, y:y_train})
+                print(ep, "  MSE:", mse)
+
+        y_pred = sess.run(outputs, feed_dict={X:X_test})
+        # print(y_pred)
+
+    plt.title("Forecast vs Actual", fontsize=14)
+    plt.plot(pd.Series(np.ravel(y_test)), 'bo', markersize=10, label="Actual")
+    plt.plot(pd.Series(np.ravel(y_pred)), 'r.', markersize=10, label="Forecast")
+    plt.legend(loc="upper left")
+    plt.xlabel("Time Periods")
+
+    return losses, y_pred
