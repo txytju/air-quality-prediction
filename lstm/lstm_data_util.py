a51067055d50c000596f38579ec8c3df9dd722a5
txytju <xingye_tian@tju.edu.cn>
2018-04-19 15:54:12 +0800

    [lstm]

--- lstm/lstm_data_util.py
+++ lstm/lstm_data_util.py
@@ -27,3 +27,27 @@ def generate_toy_data_for_lstm(num_periods = 120, f_horizon = 4, samples = 10020
     testY = TS[-(num_periods):].reshape(-1,num_periods,1)
     
     return x_batches, y_batches, testX, testY
+
+
+def generate_data_for_lstm(ts, num_periods = 120, f_horizon = 4):
+    '''
+    ts : time series to be used.
+    '''
+    
+    TS = np.array(ts)
+
+    x_data = TS[:(len(TS)-(len(TS) % num_periods))]
+    y_data = TS[f_horizon : (len(TS)-(len(TS) % num_periods)+f_horizon)]
+    print("length of training data x : ", x_data.shape)
+    print("length of training data y : ", y_data.shape)
+
+    x_batches = x_data.reshape(-1,num_periods,1)
+    y_batches = y_data.reshape(-1,num_periods,1)
+
+    print("training data x shape : ", x_batches.shape)
+    
+    test_x_setup = TS[-(num_periods + f_horizon):]
+    testX = test_x_setup[:num_periods].reshape(-1,num_periods,1) 
+    testY = TS[-(num_periods):].reshape(-1,num_periods,1)
+    
+    return x_batches, y_batches, testX, testY
