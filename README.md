135130873d7b049609cdf37946aceed0fc06b2af
txy <xingye_tian@tju.edu.cn>
2018-04-24 16:47:19 +0800

    [data_preprocess] finish data preprocess on aq data.

--- README.md
+++ README.md
@@ -16,4 +16,13 @@ The home page of this project is [here](https://www.notion.so/tianxingye/KDD-Cup
   - 增加了单变量和多变量 seq2seq 模型
 - 20180419 v4
   - 实现了 lstm 和 seq2seq 的多变量模型，模型仅考虑空气质量数据
+- 20180424 v5
+  - 天气数据
+    - 将天气数据增加到 input_feature 中
+    - 针对某空气质量站点，使用距离该站点最近的网格天气站点数据
+  - 空气质量数据缺失值的处理
+    - 数据的载入与数据处理流程分离
+      - util.data_util.parse_bj_aq_data -> util.data_util_load_bj_aq_data
+    - 对空气质量数据的不同缺失情况进行了相应的处理
+    - 建立 .csv 文件用于存放处理后的数据
 
--- utils/data_util.py
+++ utils/data_util.py
@@ -3,67 +3,73 @@ import pandas as pd
 import datetime
 from matplotlib import pyplot as plt
 
-def parse_bj_aq_data(fill_method="ffill"):
+def load_bj_aq_data():
+
+	# load csvs 
 	bj_aq_dataset_1 = pd.read_csv("./KDD_CUP_2018/Beijing/aq/beijing_17_18_aq.csv")
 	bj_aq_dataset_2 = pd.read_csv("./KDD_CUP_2018/Beijing/aq/beijing_201802_201803_aq.csv")
 	bj_aq_dataset = pd.concat([bj_aq_dataset_1, bj_aq_dataset_2], ignore_index=True)
 
-	# 将 string 类型的日期转换为 datetime 类型
-	length = bj_aq_dataset.shape[0]
-	formet_time = pd.Series([datetime.datetime.strptime(bj_aq_dataset["utc_time"][i],'%Y-%m-%d %H:%M:%S') for i in range(length)])
-	bj_aq_dataset["format_time"] = formet_time
-	bj_aq_dataset.set_index("format_time", inplace=True)
 
+	# turn date from string type to datetime type
+	bj_aq_dataset["time"] = pd.to_datetime(bj_aq_dataset['utc_time'])
+	bj_aq_dataset.set_index("time", inplace=True)
+	bj_aq_dataset.drop("utc_time", axis=1, inplace=True)
 
-	# NaN in dataset
-	pm25_nan = sum(bj_aq_dataset["PM2.5"].isnull())
-	pm10_nan = sum(bj_aq_dataset["PM10"].isnull())
-	no2_nan = sum(bj_aq_dataset["NO2"].isnull())
-	co_nan = sum(bj_aq_dataset["CO"].isnull())
-	o3_nan = sum(bj_aq_dataset["O3"].isnull())
-	so2_nan = sum(bj_aq_dataset["SO2"].isnull())
-	num_rows = bj_aq_dataset.shape[0]
-
-	print("NaN in PM2.5 is %d, %7.6f %%" %(pm25_nan, 100 * pm25_nan/num_rows))
-	print("NaN in PM10 is %d, %7.6f %%" %(pm10_nan, 100 * pm10_nan/num_rows))
-	print("NaN in NO2 is %d, %7.6f %%" %(no2_nan, 100 * no2_nan/num_rows))
-	print("NaN in CO is %d, %7.6f %%" %(co_nan, 100 * co_nan/num_rows))
-	print("NaN in O3 is %d, %7.6f %%" %(o3_nan, 100 * o3_nan/num_rows))
-	print("NaN in SO2 is %d, %7.6f %%" %(so2_nan, 100 * so2_nan/num_rows))
-
-	# 所有的站的名字
+	# names of all stations
 	stations = set(bj_aq_dataset['stationId'])
-	# type(bj_aq_data['stationId'])
-	print("There are %d air quality stations in Beijing" %len(stations))
-	print("\nThe stations in Beijing are:\n",stations)
 
 	# a dict of station aq, Beijing
 	bj_aq_stations = {}
-	bj_aq_stations_noname = {}
 	for station in stations:
-		bj_aq_station = bj_aq_dataset[bj_aq_dataset["stationId"]==station]
-		# bj_aq_station.set_index("format_time", inplace=True)
-		bj_aq_station.drop("utc_time", axis=1, inplace=True)
+		bj_aq_station = bj_aq_dataset[bj_aq_dataset["stationId"]==station].copy()
 		bj_aq_station.drop("stationId", axis=1, inplace=True)
 
 		# rename
 		original_names = bj_aq_station.columns.values.tolist()
 		names_dict = {original_name : station+"_"+original_name for original_name in original_names}
 		bj_aq_station_renamed = bj_aq_station.rename(index=str, columns=names_dict)
-
-		# fill NaN
-		if fill_method == "ffill":
-			bj_aq_station_renamed.fillna(method="ffill", inplace=True)
-                
-		# assert not np.any(np.isnan(bj_aq_station_renamed))
+              
 		bj_aq_stations[station] = bj_aq_station_renamed
 
 
-	# 整合成一个大的 dataframe
+	# merge data of different stations into one df
 	bj_aq_stations_merged = pd.concat(list(bj_aq_stations.values()), axis=1)
-	# bj_aq_stations_merged.fillna(method="ffill")
-	# assert not bj_aq_stations_merged.isnull().values.any()
+	# add a column of (0,1,2,3,...) to count
+	length = bj_aq_stations_merged.shape[0]
+	order = range(length)
+	bj_aq_stations_merged['order'] = pd.Series(order, index=bj_aq_stations_merged.index)
+
 	return bj_aq_dataset, stations, bj_aq_stations, bj_aq_stations_merged
+
+
+def data_perprocess():
+
+	# NaN in dataset
+	# pm25_nan = sum(bj_aq_dataset["PM2.5"].isnull())
+	# pm10_nan = sum(bj_aq_dataset["PM10"].isnull())
+	# no2_nan = sum(bj_aq_dataset["NO2"].isnull())
+	# co_nan = sum(bj_aq_dataset["CO"].isnull())
+	# o3_nan = sum(bj_aq_dataset["O3"].isnull())
+	# so2_nan = sum(bj_aq_dataset["SO2"].isnull())
+	# num_rows = bj_aq_dataset.shape[0]
+
+	# print("NaN in PM2.5 is %d, %7.6f %%" %(pm25_nan, 100 * pm25_nan/num_rows))
+	# print("NaN in PM10 is %d, %7.6f %%" %(pm10_nan, 100 * pm10_nan/num_rows))
+	# print("NaN in NO2 is %d, %7.6f %%" %(no2_nan, 100 * no2_nan/num_rows))
+	# print("NaN in CO is %d, %7.6f %%" %(co_nan, 100 * co_nan/num_rows))
+	# print("NaN in O3 is %d, %7.6f %%" %(o3_nan, 100 * o3_nan/num_rows))
+	# print("NaN in SO2 is %d, %7.6f %%" %(so2_nan, 100 * so2_nan/num_rows))
+
+	return
+
+
+
+
+
+
+
+
 
 
 def generate_model_data(merged_data, m, X_hours, Y_hours = 48, step=1):
