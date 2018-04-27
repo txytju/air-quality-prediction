4838d791c4e883b0ea179cae86a7899f99eb6112
txy <xingye_tian@tju.edu.cn>
2018-04-27 14:44:26 +0800

    [metrics] added metrics computation method of new seq2seq model.

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -32,14 +32,14 @@ def symmetric_mean_absolute_percentage_error(actual, forecast, y_mean, y_std):
     return r/length
 
 
-def SMAPE_on_dataset(actual_data, forecast_data, feature_list, y_mean, y_std, forecast_duration=24):
+def SMAPE_on_dataset(actual_data, forecast_data, feature_list, y_mean, y_std, forecast_duration=1):
     '''
     Compute SMAPE value on the dataset of actual and forecast.
 	
     Args:
         actual_data : actual data in the dev set or test set, shape [number_of_examples_in_the_dev_set, output_seq_length, num_of_output_features]
         forecast_data : forecast data which are predicted by the seq2seq model using x data in the dev set or test set, same shape as actual_data.
-        forecast_duration : predict every 24 hours, so forecast_duration is set default to 24.
+        forecast_duration : predict every 24 hours, so forecast_duration is set default to 1, because the dev set is sampled every 24 hours.
         feature_list : a list of features that is caculated in the forecast.
     Return:
         aver_smapes : average smape for all features on the test data.
@@ -66,7 +66,41 @@ def SMAPE_on_dataset(actual_data, forecast_data, feature_list, y_mean, y_std, fo
     return aver_smapes, smapes_of_features
     
         
+# For new seq2seq model
+def SMAPE_on_dataset_v1(actual_data, forecast_data, feature_list, statistics, forecast_duration=1):
+    '''
+    Compute SMAPE value on the dataset of actual and forecast.
+    
+    Args:
+        actual_data : actual data in the dev set or test set, shape [number_of_examples_in_the_dev_set, output_seq_length, num_of_output_features]
+        forecast_data : forecast data which are predicted by the seq2seq model using x data in the dev set or test set, same shape as actual_data.
+        forecast_duration : predict every 24 hours, so forecast_duration is set default to 1, because the dev set is sampled every 24 hours.
+        feature_list : a list of features that is caculated in the forecast. Need to be in the right order!!
+        statistics : a pandas dataframe of statistics.
+    Return:
+        aver_smapes : average smape for all features on the test data.
+        smapes_of_features : smapes of different features on the test data.
+    '''
+    assert actual_data.shape == forecast_data.shape, "The shape of actual data and perdiction data must match."
 
+    number_of_features = actual_data.shape[2]
+    smapes_list_of_features = {feature:[] for feature in feature_list}
 
+    for i in range(0, actual_data.shape[0], forecast_duration):
+        actual_data_item = actual_data[i]
+        forecast_data_item = forecast_data[i]
+        for j in range(number_of_features):
+            feature = feature_list[j]
+            a = actual_data_item[:,j]
+            f = forecast_data_item[:,j]
+            y_mean = statistics.loc['mean'][feature]
+            y_std = statistics.loc['std'][feature]
+            smape_a_feature_a_day = symmetric_mean_absolute_percentage_error(a, f, y_mean, y_std)
+            smapes_list_of_features[feature].append(smape_a_feature_a_day)
+
+    smapes_of_features = {feature:np.mean(value) for feature, value in smapes_list_of_features.items()}
+    aver_smapes = np.mean(list(smapes_of_features.values()))
+
+    return aver_smapes, smapes_of_features
 
 
