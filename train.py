49ba5bec358c2ff84946b5cb98c435a495dca9b4
txy <xingye_tian@tju.edu.cn>
2018-04-28 23:14:13 +0800

    [py]

--- train.py
+++ train.py
@@ -14,23 +14,46 @@ gap = 0
 # 基于上述下载的数据，进行数据预处理，并将生成的中间数据保存在 csv 表格中
 
 aq_data_preprocess(city='bj')
-aq_data_preprocess(city='ld')
+# aq_data_preprocess(city='ld')
 meo_data_preprocess(city='bj')
-meo_data_preprocess(city='ld')
+# meo_data_preprocess(city='ld')
 
 # 3. 训练集验证集划分
 train_dev_set_split(city="bj")
-train_dev_set_split(city="ld")
+# train_dev_set_split(city="ld")
 
 # 4. 训练模型
 pred_days_list = [3,5,7,10]
 loss_functions = ["L2", "L1", "huber_loss"]
 
-for pred_days in pred_days_list : 
+for pred_days in pred_days_list :
+
+    # pred_days 不同生成的 dev set 也不同
+    test_x, test_y = generate_dev_set(city=city,
+                                  station_list=station_list,
+                                  X_aq_list=X_aq_list, 
+                                  y_aq_list=y_aq_list, 
+                                  X_meo_list=X_meo_list,
+                                  pre_days=pre_days,
+                                  gap=gap)
     for loss_function in loss_functions :
         model_preds, model_name = train_and_dev(city='bj', pre_days=pred_days, gap=gap, loss_function=loss_function)
+
+
+# 5. 模型融合
+
+
+# 6. 使用融合的模型预测
+
+
+
+# 7. 自动提交结果
+
+
+
 
 
 
 # TODO：
 # 1. 每天定时跑，并且将时间和 gap 变量对应上
+# 2. 当前 model_preds 是在正则化数据上的，将之转换到原始数据分布 statistics
